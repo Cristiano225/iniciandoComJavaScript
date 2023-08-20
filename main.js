3b8c73c28f5392baa6f6c4e25df67bf5ae1afc6e
Cristiano Marques <cristianoms225@gmail.com>
2023-08-20 18:27:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+// 1)
+alert("Hello world");
+
+// 2)
+alert(5 + 5);
+
+// 3)
+let isNumber = 5;
+
+if (typeof isNumber == 'number') {
+  alert("It is a " + typeof isNumber);
+}
+else {
+  alert("It is not a " + typeof isNumber);
+}
+
+// 4)
+let nString = "abacate";
+
+if (typeof nString == "string") {
+  alert("It is a " + typeof nString);
+}
+else {
+  alert("It is not a " + typeof nString);
+}
+
+// 5)
+
+let nBoolean = true;
+
+if (typeof nBoolean == "boolean") {
+  alert("It is a " + typeof nBoolean);
+} 
+else {
+  alert("It is not a " + typeof nBoolean);
+}
+
+ // 6)
+let nSoma1 = 10;
+let nSoma2 = 20;
+let result = nSoma2 - nSoma1;
+
+alert(resultSoma);
+
+ // 7)
+let nMult1 = 10;
+let nMult2 = 20;
+let resultMult = nMult1 * nMult2;
+
+alert(resultMult);
+
+ // 8)
+
+let nDiv1 = 10;
+let nDiv2 = 20;
+let resultDiv = nDiv2 * nDiv1;
+
+alert(resultDiv);
+
+ // 9)
+
+let isPair = 2;
+
+if (isPair % 2 == 0) {
+  alert("O número é par");
+} 
+else {
+  alert("O número é impar");
+}
+
+ // 10)
+
+let isOdd = 2;
+
+if (isOdd % 2 != 0) {
+  alert("O número é impar");
+} 
+else {
+  alert("O número é par");
+}
